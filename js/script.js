42c0c33b8c610fe8d724ad062f03c641dacde677
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-05-26 01:26:06 +0300

    feat: timer obratnogo otcheta

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 window.addEventListener('DOMContentLoaded',() =>{
+    //Tabs implemetation 
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -35,4 +36,59 @@ window.addEventListener('DOMContentLoaded',() =>{
             });
         }
     });
+    //Tabs implemetation  end 
+
+    //Timer implementation
+
+    const deadLine = '2022-06-01';
+    function getTimeRemaining(endTime){
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000*60*60*24)),
+            hours = Math.floor(t / (1000*60*60)%24),   
+            minutes = Math.floor(t / (1000*60)%60),    
+            seconds = Math.floor(t / (1000)%60);  
+            
+        return {
+            'time': t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds 
+        };
+    }
+
+    function correctNum(num){
+        if(num>=0 && num <10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+
+    }
+    function clockRefresh(selector , endTime){
+        const timeWrapper = document.querySelector(selector),
+                daysSel = timeWrapper.querySelector('#days'),
+                hoursSel = timeWrapper.querySelector('#hours'),
+                minutesSel = timeWrapper.querySelector('#minutes'),
+                secondsSel = timeWrapper.querySelector('#seconds'),
+                timeInterval = setInterval(updateClock,1000);
+        updateClock();
+
+        function updateClock(){
+            const remainingTotalTime = getTimeRemaining(endTime);
+
+            daysSel.innerHTML=correctNum(remainingTotalTime.days);
+            hoursSel.innerHTML=correctNum(remainingTotalTime.hours);
+            minutesSel.innerHTML=correctNum(remainingTotalTime.minutes);
+            secondsSel.innerHTML=correctNum(remainingTotalTime.seconds);
+    
+            if(remainingTotalTime.t<=0){
+                clearInterval(timeInterval);
+            }
+        }
+
+
+    }
+    clockRefresh('.timer', deadLine);
+    //Timer implementation end
 });
